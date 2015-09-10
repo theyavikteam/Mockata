c0d9647c82996f149dbd2470f080b7774441bcf8
yavik14 <yavik14@gmail.com>
2015-09-11 00:25:43 +0200

    Initial Commit

---
+++ app/src/main/java/com/theyaviktea/mockatadroid/MockataActivityFragment.java
@@ -0,0 +1,22 @@
+package com.theyaviktea.mockatadroid;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MockataActivityFragment extends Fragment {
+
+    public MockataActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_mockata, container, false);
+    }
+}
